4bdbc0750fc0c91004c9ec1324e1b54c9e64652f
DanioFiore <daniofiore@hotmail.com>
2022-10-28 12:33:48 +0200

    added promises

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 const fetchPokemon = () => {
 
+    const promises = []
     for (let i = 1; i <= 150; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        fetch(url)
+
+        promises.push(fetch(url)
             .then(res => {
                 return res.json();
-            })
-            .then(data => {
-                console.log(data);
-                const pokemon = {
-                    'id': data.id,
-                    'name': data.name,
-                    'sprite': data.sprites['front_default'],
-                    'type': data.types.map(element =>
-                        element.type.name
-
-                    ).join(', ')
-                };
-                console.log(pokemon);
-            })
+            }))
     }
 
+    Promise.all(promises).then(results => {
+        const pokemon = results.map(data => ({
+            'id': data.id,
+            'name': data.name,
+            'sprite': data.sprites['front_default'],
+            'type': data.types.map(element =>
+                element.type.name
+
+            ).join(', ')
+        }))
+        console.log(pokemon)
+    })
 }
 
+const displayPokemon = (pokemon) => {
+    console.log(pokemon)
+}
 fetchPokemon();
